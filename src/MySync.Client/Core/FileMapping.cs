d11f515ff75c101ccc5fdc2a193dd3232a9fde25
Damian Korczowski <erdroy2@gmail.com>
2016-12-03 17:28:30 +0100

    Added file 'version difference calculator' methods.

--- src/MySync.Client/Core/FileMapping.cs
+++ src/MySync.Client/Core/FileMapping.cs
@@ -1,21 +1,49 @@
 ﻿// MySync © 2016 Damian 'Erdroy' Korczowski
 // under GPL-3.0 license
 
+using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using Newtonsoft.Json;
 
 namespace MySync.Client.Core
 {
     public class FileMapping
     {
-        public struct FileEntry
+        public struct FileEntry : IEquatable<FileEntry>
         {
             public string File;
             public long Version;
+
+            public bool Equals(FileEntry other)
+            {
+                return File == other.File;
+            }
         }
 
         public List<FileEntry> Files = new List<FileEntry>();
+
+        public void Update(string rootDirectory)
+        {
+            var files = Directory.EnumerateFiles(rootDirectory, "*.*", SearchOption.AllDirectories);
+
+            Files.Clear();
+            foreach (var file in files)
+            {
+                var fileinfo = new FileInfo(file);
+                Files.Add(new FileEntry
+                {
+                    File = file,
+                    Version = fileinfo.LastWriteTime.ToBinary()
+                });
+            }
+        }
+        
+        public string ToJson()
+        {
+            return JsonConvert.SerializeObject(this, Formatting.Indented);
+        }
 
         public static FileMapping CreateFileMapping(string rootDirectory)
         {
@@ -36,6 +64,41 @@ namespace MySync.Client.Core
             }
 
             return mapping;
+        }
+
+        public static List<FileEntry> GetChangedFiles(FileMapping localMapping, FileMapping remoteMapping)
+        {
+            return localMapping.Files.Where(p => remoteMapping.Files.Any(l => p.File == l.File && p.Version != l.Version)).ToList();
+        }
+
+        public static List<FileEntry> GetNewFiles(FileMapping localMapping, FileMapping remoteMapping)
+        {
+            var changedFiles = GetChangedFiles(localMapping, remoteMapping);
+            var newFiles = localMapping.Files.Except(remoteMapping.Files).ToList();
+
+            // remove changed files
+            foreach (var file in changedFiles)
+            {
+                if (newFiles.Contains(file))
+                    newFiles.Remove(file);
+            }
+
+            return newFiles;
+        }
+
+        public static List<FileEntry> GetDeletedFiles(FileMapping localMapping, FileMapping remoteMapping)
+        {
+            var newFiles = GetNewFiles(localMapping, remoteMapping);
+            var deletedFiles = remoteMapping.Files.Except(localMapping.Files).ToList();
+            
+            // remove changed files
+            foreach (var file in newFiles)
+            {
+                if (deletedFiles.Contains(file))
+                    deletedFiles.Remove(file);
+            }
+
+            return deletedFiles;
         }
 
         public static FileMapping FromJson(string jsonfile)
