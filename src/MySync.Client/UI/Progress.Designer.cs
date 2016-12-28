6cafb793c42f154d72cb4da41c9994adce3640ad
Damian Korczowski <erdroy2@gmail.com>
2016-12-28 19:23:18 +0100

    added progresswindow window.

---
+++ src/MySync.Client/UI/Progress.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace MySync.Client.UI
+{
+    partial class Progress
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.title = new MetroFramework.Controls.MetroLabel();
+            this.metroProgressSpinner1 = new MetroFramework.Controls.MetroProgressSpinner();
+            this.message = new MetroFramework.Controls.MetroLabel();
+            this.SuspendLayout();
+            // 
+            // title
+            // 
+            this.title.AutoSize = true;
+            this.title.Location = new System.Drawing.Point(23, 13);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(33, 19);
+            this.title.TabIndex = 0;
+            this.title.Text = "Title";
+            // 
+            // metroProgressSpinner1
+            // 
+            this.metroProgressSpinner1.Location = new System.Drawing.Point(23, 35);
+            this.metroProgressSpinner1.Maximum = 100;
+            this.metroProgressSpinner1.Name = "metroProgressSpinner1";
+            this.metroProgressSpinner1.Size = new System.Drawing.Size(40, 40);
+            this.metroProgressSpinner1.TabIndex = 1;
+            this.metroProgressSpinner1.UseSelectable = true;
+            this.metroProgressSpinner1.Value = 20;
+            // 
+            // message
+            // 
+            this.message.AutoSize = true;
+            this.message.Location = new System.Drawing.Point(69, 45);
+            this.message.Name = "message";
+            this.message.Size = new System.Drawing.Size(60, 19);
+            this.message.TabIndex = 2;
+            this.message.Text = "Message";
+            // 
+            // Progress
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 106);
+            this.Controls.Add(this.message);
+            this.Controls.Add(this.metroProgressSpinner1);
+            this.Controls.Add(this.title);
+            this.DisplayHeader = false;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(500, 106);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(500, 106);
+            this.Name = "Progress";
+            this.Padding = new System.Windows.Forms.Padding(20, 30, 20, 20);
+            this.Resizable = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Style = MetroFramework.MetroColorStyle.Orange;
+            this.Text = "Message";
+            this.Load += new System.EventHandler(this.Progress_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroLabel title;
+        private MetroFramework.Controls.MetroProgressSpinner metroProgressSpinner1;
+        private MetroFramework.Controls.MetroLabel message;
+    }
+}
---
+++ src/MySync.Client/UI/Progress.cs
@@ -0,0 +1,51 @@
+﻿// MySync © 2016 Damian 'Erdroy' Korczowski
+
+using System;
+using MetroFramework.Forms;
+
+namespace MySync.Client.UI
+{
+    public partial class Progress : MetroForm
+    {
+        public static Progress Current;
+
+        public Progress()
+        {
+            InitializeComponent();
+        }
+
+        private void Progress_Load(object sender, System.EventArgs e)
+        {
+
+        }
+
+        public static void ShowWindow(string title)
+        {
+            if(Current != null)
+                throw new Exception("Cannot show second window.");
+
+            // show window
+            Current = new Progress
+            {
+                title =
+                {
+                    Text = title
+                }
+            };
+            MainWindow.Instance.AddOwnedForm(Current);
+            Current.Show();
+        }
+
+        public static void CloseWindow()
+        {
+            Current.Close();
+            Current = null;
+        }
+        
+        public static string Message
+        {
+            get { return Current.message.Text; }
+            set { Current.message.Text = value; }
+        }
+    }
+}
