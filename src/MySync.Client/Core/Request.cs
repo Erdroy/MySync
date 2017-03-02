93b114f593487a8d336fcfcfd1c2fba1ec019c45
Damian Korczowski <erdroy2@gmail.com>
2017-03-02 16:30:41 +0100

    added request class

---
+++ src/MySync.Client/Core/Request.cs
@@ -0,0 +1,39 @@
+﻿// MySync © 2016-2017 Damian 'Erdroy' Korczowski
+
+using System;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace MySync.Client.Core
+{
+    /// <summary>
+    /// Request class.
+    /// Allows POST HTTP requests.
+    /// </summary>
+    public static class Request
+    {
+        /// <summary>
+        /// Send HTTP POST request.
+        /// </summary>
+        /// <param name="address">The full URL addres.</param>
+        /// <param name="body">The JSON body.</param>
+        /// <param name="callback">The callback stream.</param>
+        public static void Send(string address, string body, Action<Stream> callback)
+        {
+            var request = WebRequest.Create(address);
+            request.Credentials = CredentialCache.DefaultCredentials;
+            request.Method = "POST";
+            request.ContentType = "application/json";
+
+            var data = Encoding.UTF8.GetBytes(body);
+            request.ContentLength = data.Length;
+            var dataStream = request.GetRequestStream();
+            dataStream.Write(data, 0, data.Length);
+            dataStream.Close();
+
+            var response = request.GetResponse();
+            callback(response.GetResponseStream());
+        }
+    }
+}
--- src/MySync.Client/Program.cs
+++ src/MySync.Client/Program.cs
@@ -1,5 +1,10 @@
 ﻿// MySync © 2016-2017 Damian 'Erdroy' Korczowski
 
+using System;
+using System.Net;
+using System.Threading;
+using MySync.Client.Core;
+
 namespace MySync.Client
 {
     internal static class Program
@@ -7,7 +12,17 @@ namespace MySync.Client
         private static void Main()
         {
             // initialize
+            var body = "{\n\t\"username\":\"testUser\",\n\t\"password\":\"235t2b62626256236n2===\"\n}";
+            Request.Send("http://127.0.0.1:8080/authorize", body, stream =>
+            {
+                using (var reader = new System.IO.StreamReader(stream))
+                {
+                    Console.WriteLine(reader.ReadToEnd());
+                }
+            });
 
+            while (true)
+                Thread.Sleep(100);
         }
     }
 }
