47df7a8bb4a97ebb84db101adda92d9b3a2ef169
Damian Korczowski <erdroy2@gmail.com>
2017-03-08 11:57:21 +0100

    commit model now can be converted to commit structure

--- src/MySync.Server/Core/DatabaseModels/CommitModel.cs
+++ src/MySync.Server/Core/DatabaseModels/CommitModel.cs
@@ -1,5 +1,7 @@
 ﻿// MySync © 2016-2017 Damian 'Erdroy' Korczowski
 
+using MySync.Shared.VersionControl;
+
 namespace MySync.Server.Core.DatabaseModels
 {
     /// <summary>
@@ -21,5 +23,30 @@ namespace MySync.Server.Core.DatabaseModels
         public string CommitDescription { get; set; }
 
         public FileDiff[] Files { get; set; }
+        
+        /// <summary>
+        /// Convert commit model to commit structure.
+        /// </summary>
+        /// <returns>The created commit structure.</returns>
+        public Commit ToCommit()
+        {
+            var commit = new Commit
+            {
+                Description = CommitDescription,
+                Files = new Filemap.FileDiff[Files.Length]
+            };
+
+            for (var i = 0; i < Files.Length; i++)
+            {
+                commit.Files[i] = new Filemap.FileDiff
+                {
+                    FileName = Files[i].Name,
+                    DiffType = (Filemap.FileDiff.Type)Files[i].Operation,
+                    Version = Files[i].Version
+                };
+            }
+
+            return commit;
+        }
     }
 }
