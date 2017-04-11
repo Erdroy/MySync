022b902ce3b9b9359468264c5b3e0dc4b8f088de
Damian Korczowski <erdroy2@gmail.com>
2017-04-11 15:52:48 +0200

    UI progress  - based on CEF - HTML5!

---
+++ src/MySync/MainWindow.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MySync
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MainWindow_Load(object sender, EventArgs e)
+        {
+            Controls.Add(Program.Browser);
+        }
+    }
+}
---
+++ src/MySync/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Security.Policy;
+using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
+
+namespace MySync
+{
+    /// <summary>
+    /// Program class.
+    /// </summary>
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        private static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Cef.EnableHighDPISupport();
+
+            var settings = new CefSettings
+            {
+                RemoteDebuggingPort = 8088,
+                CachePath = "cache",
+                MultiThreadedMessageLoop = true,
+                ExternalMessagePump = false,
+                FocusedNodeChangedEnabled = true
+            };
+
+            settings.CefCommandLineArgs.Add("no-proxy-server", "1");
+
+            if (!Cef.Initialize(settings))
+            {
+                throw new Exception("Unable to Initialize Cef");
+            }
+
+            var uriBuilder = new UriBuilder(Environment.CurrentDirectory.Replace('\\', '/') + "/ui/index.html");
+            var uri = uriBuilder.Uri.ToString();
+            Browser = new ChromiumWebBrowser(uri)
+            {
+                Dock = DockStyle.Fill
+            };
+
+            Application.Run(new MainWindow());
+        }
+
+        /// <summary>
+        /// The default browser used to render UI.
+        /// </summary>
+        public static ChromiumWebBrowser Browser { get; private set; }
+    }
+}
