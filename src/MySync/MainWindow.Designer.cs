d442c308e07cb9a55067ec230cd6cde796d685b5
Damian Korczowski <erdroy2@gmail.com>
2017-04-13 18:58:10 +0200

    added minimal window size and new/open project buttons

--- src/MySync/MainWindow.Designer.cs
+++ src/MySync/MainWindow.Designer.cs
@@ -34,8 +34,9 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.ClientSize = new System.Drawing.Size(800, 600);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MinimumSize = new System.Drawing.Size(800, 600);
             this.Name = "MainWindow";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "MySync";
