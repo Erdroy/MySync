1597a210c0024a72801d85a2973833e47214068b
Damian Korczowski <erdroy2@gmail.com>
2017-04-16 14:42:54 +0200

    added buttons pull/push/discard handlers

--- src/MySync/Projects.cs
+++ src/MySync/Projects.cs
@@ -1,24 +1,44 @@
 ﻿// MySync © 2016-2017 Damian 'Erdroy' Korczowski
 
+using System;
+using CefSharp;
+
 namespace MySync
 {
     public class Projects
     {
+        private IFrame _frame;
+
         private static void RunJs(string js)
         {
             Program.Browser.GetBrowser().MainFrame.ExecuteJavaScriptAsync(js);
-
         }
 
         public void LoadProjects()
         {
-            RunJs("clearFileChanges();");
+            _frame = Program.Browser.GetBrowser().MainFrame;
 
+            RunJs("clearFileChanges();");
         }
 
         public void SelectProject(string projectName)
         {
             
+        }
+        
+        public void Discard()
+        {
+
+        }
+
+        public void Pull()
+        {
+
+        }
+
+        public void Push()
+        {
+
         }
     }
 }
